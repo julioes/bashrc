b24afeaa1852c80d7e6a67d5ce10c390b8d68154
Julio Espinoza-Sokal <julioes@gmail.com>
2012-07-11 22:19:39 -0400

    Add android mount function and move rvm path into tracked files.
    Signed-off-by: Julio Espinoza-Sokal <julioes@gmail.com>

---
+++ albion.home/bash_functions.bash
@@ -0,0 +1,4 @@
+
+# mount the android build image
+function mountAndroid { hdiutil attach /Developer/android.dmg.sparseimage -mountpoint /Volumes/android; }
+
--- bash_profile.bash
+++ bash_profile.bash
@@ -1,3 +1,11 @@
 # Load RVM function
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
+if [ -d $HOME/.rvm/bin ]; then
+  PATH=$PATH:$HOME/.rvm/bin
+fi
+
+if [ -d ~/bin ]; then
+  PATH=~/bin:$PATH
+fi
+
