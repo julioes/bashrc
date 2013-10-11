a68efa1b529b3d06a1330ac0701bff9517d2820f
Julio Espinoza-Sokal <julioes@gmail.com>
2013-10-10 22:08:40 -0400

    Fix warning in RVM for not being first in PATH
    Signed-off-by: Julio Espinoza-Sokal <julioes@gmail.com>

--- bash_profile.bash
+++ bash_profile.bash
@@ -1,11 +1,11 @@
+if [ -d ~/bin ]; then
+  PATH=~/bin:$PATH
+fi
+
 # Load RVM function
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
 if [ -d $HOME/.rvm/bin ]; then
   PATH=$PATH:$HOME/.rvm/bin
-fi
-
-if [ -d ~/bin ]; then
-  PATH=~/bin:$PATH
 fi
 
